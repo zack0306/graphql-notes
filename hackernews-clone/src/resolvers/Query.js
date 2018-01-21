2acc5e31eee96e8532da60603fe45791ab5778fd
Julien Panopio <jcpanopio1@gmail.com>
2018-01-21 11:08:55 -0800

    Fix feed query filter

--- hackernews-clone/src/resolvers/Query.js
+++ hackernews-clone/src/resolvers/Query.js
@@ -1,7 +1,7 @@
 function feed(parent, args, context, info) {
   const { filter, first, skip } = args;
   const where = filter
-    ? { OR: [{ urlContains: filter }, { descriptionContains: filter }] }
+    ? { OR: [{ url_contains: filter }, { description_contains: filter }] }
     : {};
 
   return context.db.query.links({ first, skip, where }, info);
