98d99e2653605ce1ad2d073ce5414f78ca36afd0
Julien Panopio <jcpanopio1@gmail.com>
2018-01-21 11:34:26 -0800

    Change feed Query declaration, fix typo in index.js

--- hackernews-clone/src/index.js
+++ hackernews-clone/src/index.js
@@ -1,7 +1,7 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { Prisma } = require('prisma-binding');
 const Query = require('./resolvers/Query');
-const Mutation = require('./reoslvers/Mutation');
+const Mutation = require('./resolvers/Mutation');
 
 const resolvers = {
   Query,
--- hackernews-clone/src/resolvers/Query.js
+++ hackernews-clone/src/resolvers/Query.js
@@ -1,10 +1,10 @@
-function feed(parent, args, context, info) {
+const feed = (parent, args, context, info) => {
   const { filter, first, skip } = args;
   const where = filter
     ? { OR: [{ url_contains: filter }, { description_contains: filter }] }
     : {};
 
   return context.db.query.links({ first, skip, where }, info);
-}
+};
 
 module.exports = { feed };
